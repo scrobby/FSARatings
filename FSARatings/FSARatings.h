b6962f4c592fdef5febdffcd6535c9f9bd63d953
Carl Goldsmith <carl.goldsmith@me.com>
2016-08-21 11:17:10 +0100

    Initial Commit

---
+++ FSARatings/FSARatings.h
@@ -0,0 +1,19 @@
+//
+//  FSARatings.h
+//  FSARatings
+//
+//  Created by Carl Goldsmith on 21/08/2016.
+//  Copyright © 2016 carlgoldsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FSARatings.
+FOUNDATION_EXPORT double FSARatingsVersionNumber;
+
+//! Project version string for FSARatings.
+FOUNDATION_EXPORT const unsigned char FSARatingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSARatings/PublicHeader.h>
+
+
